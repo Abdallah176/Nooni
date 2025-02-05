a205f0b13317e976ee1fa2bfb89881e5282a6aac
Abdallah176 <abdalaagamal226@gmail.com>
2025-02-05 04:10:02 +0200

    05/2/2025

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,18 @@
 // VideoHeroSection
-document.querySelector('.video-icon').addEventListener('click', function() {
-    const video = document.getElementById('myVideo');
-    if (video.style.display === 'block') {
-        video.style.display = 'none';  
-        } else {
-        video.style.display = 'block';  
-        video.play(); 
-        }
-    });
+// document.querySelector('.video-icon').addEventListener('click', function() {
+//     const video = document.getElementById('myVideo');
+//     if (video.style.display === 'block') {
+//         video.style.display = 'none';  
+//         } else {
+//         video.style.display = 'block';  
+//         video.play(); 
+//         }
+//     });
 
-
-  // const items = document.querySelectorAll('.marquee-item');
-  // let currentIndex = 0;
-
-  // function showNextItem() {
-  //     // إزالة الـ active من العنصر الحالي
-  //     items[currentIndex].classList.remove('active');
-  //     items[currentIndex].classList.add('exit');
-
-  //     // التحديث إلى العنصر التالي
-  //     currentIndex = (currentIndex + 1) % items.length;
-
-  //     // إظهار العنصر التالي
-  //     items[currentIndex].classList.remove('exit');
-  //     items[currentIndex].classList.add('active');
-  // }
-  //    // بدء الحركة
-  //     items[currentIndex].classList.add('active');
-  //    setInterval(showNextItem, 3000); // تبديل كل 3 ثوانٍ
-
+// Link
+function redirectToPage() {
+    window.location.href = "../register.html";
+}
 // left Shop Category 
 const section = document.getElementById('mySectionLeft');
 let hasAnimated = false; // متغير للتحقق من أن الحركة حدثت بالفعل
